f56cc419dfda954fb17e2188828da8afa398edde
Hannes <106339612+hannesprojects@users.noreply.github.com>
2022-11-20 00:27:21 +0100

    notwa

--- frontend/david/david/views/tabViews/map/stickyNotes/CreateNoteView.swift
+++ frontend/david/david/views/tabViews/map/stickyNotes/CreateNoteView.swift
@@ -8,14 +8,38 @@
 import SwiftUI
 
 struct CreateNoteView: View {
+    var note: NoteStruct
+
     var body: some View {
-        NavigationView{}
-        ZStack {
-            Image
-            
+            VStack(alignment: .leading, spacing: 8) {
+                Text(note.note_title)
+                    .modifier(TitleModifierNotes())
+                    .frame(maxWidth: 170, alignment: .leading)
+                    .layoutPriority(1)
+                
+                Text(note.note_description)
+                    .modifier(TextModifierNotes())
+                    .opacity(0.7)
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                Spacer()
+                HStack{
+                    Text("created on: " + note.note_created_at)
+                        .modifier(DescriptionModifierNotes())
+                    Text("by: " + note.note_author)
+                        .modifier(DescriptionModifierNotes())
+                }
+            }
+            .foregroundColor(Color.clear)
+            .padding(30)
+            .frame(width: UIScreen.screenWidth - 50, height: UIScreen.screenWidth )
+            .background(Color.yellowNotes)
+            .mask(RoundedRectangle(cornerRadius: 5, style: .continuous))
+            .shadow(radius: 4, x: -1, y: 5)
+     //       .shadow(radius: 2, x: 0, y: 1)
         }
-    }
+    
 }
+
 
 struct CreateNoteView_Previews: PreviewProvider {
     static var previews: some View {
