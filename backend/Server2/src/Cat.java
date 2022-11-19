74ca31a6aec87cb6c5c19753746ad4c5d1bf9684
Heidi Albarazi <heidialbarazi2306@gmail.com>
2022-11-19 11:57:45 +0100

    ui changes

--- backend/Server2/src/Cat.java
+++ backend/Server2/src/Cat.java
@@ -1,4 +1,4 @@
 public class Cat {
 
-    private int Cat;
+
 }
---
+++ frontend/david/david/model/HTTPMethod.swift
@@ -0,0 +1,16 @@
+//
+//  HTTPMethod.swift
+//  david
+//
+//  Created by Heidi Albarazi on 19.11.22.
+//
+
+
+import Foundation
+
+enum HTTPMethod: String {
+    case GET
+    case POST
+    case PUT
+    case DEL
+}
