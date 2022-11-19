b4a6a01e5590aa9ebd35ef6c3a76cccb93d082e9
Heidi Albarazi <heidialbarazi2306@gmail.com>
2022-11-19 10:11:58 +0100

    add file

---
+++ frontend/david/david/views/RandomView.swift
@@ -0,0 +1,20 @@
+//
+//  RandomView.swift
+//  david
+//
+//  Created by Heidi Albarazi on 19.11.22.
+//
+
+import SwiftUI
+
+struct RandomView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct RandomView_Previews: PreviewProvider {
+    static var previews: some View {
+        RandomView()
+    }
+}
