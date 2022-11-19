f88f1a7f766f91059830265f718c2e77ee46230f
Hannes <106339612+hannesprojects@users.noreply.github.com>
2022-11-19 09:51:15 +0100

    add files

---
+++ frontend/david/david/views/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  hackaTUM
+//
+//  Created by Hannes Wagner on 19.11.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
