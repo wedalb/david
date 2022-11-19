3c9a4f7553388feba2e2eb4f0b50d64c73adc76e
Heidi Albarazi <heidialbarazi2306@gmail.com>
2022-11-19 15:39:30 +0100

    created frontend folder

---
+++ frontend/david/david/davidApp.swift
@@ -0,0 +1,17 @@
+//
+//  davidApp.swift
+//  david
+//
+//  Created by Heidi Albarazi on 19.11.22.
+//
+
+import SwiftUI
+
+@main
+struct davidApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
