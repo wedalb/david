f8de69dfa1d783418e068b72c648e81a4c9e51c7
Heidi Albarazi <heidialbarazi2306@gmail.com>
2022-11-19 09:39:57 +0100

    add folder

--- frontend/david/david/ContentView.swift
+++ frontend/david/david/ContentView.swift
@@ -11,6 +11,14 @@ struct ContentView: View {
     var body: some View {
         VStack {
             Image(systemName: "globe")
+      
+                
+                
+                
+                
+                
+                
+                
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
             Text("Hello, world!")
